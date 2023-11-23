66c7c4c92e07499f553c9300b26fd87abf2d2991
Chinmay <rajchinmay08@gmail.com>
2023-11-24 03:20:21 +0530

    updating config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {formats: ["image/webp"] }
+}
 
 module.exports = nextConfig
--- src/app/components/SkillCard.jsx
+++ src/app/components/SkillCard.jsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 
 const SkillCard = ({ title, url }) => {
   return (
-    <div className='flex flex-col justify-center items-center hover:bg-[#181434] rounded-xl sm:ml-0 sm:mr-0 ml-[8px] mr-[8px]'>
+    <div className='flex flex-col justify-center items-center hover:bg-[#181434] hover:shadow-lg hover:shadow-[#413c64] rounded-xl sm:ml-0 sm:mr-0 ml-[8px] mr-[8px]'>
         <Image src={url} alt='Skill-Image' width={50} height={50} className='mb-8 mt-4' />
         <div className='text-white font-semibold mb-4'>
           {title}
