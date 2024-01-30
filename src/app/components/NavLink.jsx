f61343b2b4ac825638983ed69fa40ebef78cee07
Chinmay <rajchinmay08@gmail.com>
2024-01-30 20:33:41 +0530

    minor changes

--- src/app/components/NavLink.jsx
+++ src/app/components/NavLink.jsx
@@ -1,12 +1,9 @@
-import Link from 'next/link';
-import React from 'react';
-import { easeInOut, motion, spring } from 'framer-motion';
 
 const NavLink = ({ href, title, setNavbarOpen }) => {
   return (
     <a
         href={`#${href}`}
-        className='block py-2 sm:text-lg rounded md:p-0 text-violet-700'
+        className='block py-2 text-violet-700 sm:text-[#ADB7BE] sm:text-lg rounded md:p-0 hover:text-white'
         onClick={() => setNavbarOpen(false)}
     >
         {title}
